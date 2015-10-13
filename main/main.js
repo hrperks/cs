621d9043937600c3347513422d1152dc0f082e20
Herbert Perks <hperks92@gmail.com>
2015-10-12 21:20:00 -0400

    setting up functionality

--- main/main.js
+++ main/main.js
@@ -8,7 +8,48 @@ angular.module('creepScore.main',['ngRoute'])
 		controller: 'mainController'
 	})
 }])
-.controller('mainController', ['$scope','$routeParams', 'SummonerService', function($scope,$routeParams,SummonerService){
+.factory('Main',function($http,$log,$q,ChampionService){
+	return{
+		 setUpChampions: function(data){
+		 	if(ChampionService.championList.length==0){data.getChampions=true;}
+		 	$http.post('engine.php?method=route',{class:'main',function:'getChampion', data: data})
+				.then(function(success){
+					if(data.getChampions){ChampionService.setChampionList(success.data.championList.data);}
+					
+					log(success, 'main.getChampion success-');
+					p.resolve(success)
+				}, function(){
+					log(data,"main.getChampion failure -");
+
+				})
+			return p.promise
+		 }
+	}
+})
+.controller('mainController', ['$scope','$routeParams', 'SummonerService', 'Main', function($scope,$routeParams,SummonerService){
 		$scope.name = $routeParams.name;
 		$scope.region = $routeParams.region;
+		$scope.roleList=[
+		{name:"middle lane", value:"mid"},
+		{name:"attack Damage Carry", value:"adc"},
+		{name:"Top Lane", value:"top"},
+		];
+		$scope.display='';
+		$scope.testOne="MidLane";
+		$scope.testTwo="ADC";
+		$scope.testThree="TOP lane";
+		$scope.roleSel = $scope.roleList[0];
+		$scope.test = function(){
+			switch($scope.roleSel.value){
+				case "mid":
+					$scope.display=$scope.testOne;
+					break;
+				case "adc":
+					$scope.display=$scope.testTwo;
+					break;
+				case "top":
+					$scope.display=$scope.testThree;
+					break;
+			}
+		}
 }])
