41fc25e6de2a7b9c0425e9dc58da2737ca16a608
Herbert Perks <hperks92@gmail.com>
2015-10-13 16:08:27 -0400

    added "levelUp" functionality
    main idea, is to remove from the list, then place into the following
    list, Ordering is not a big deal for me as of now.

--- main/main.js
+++ main/main.js
@@ -142,12 +142,31 @@ angular.module('creepScore.main',['ngRoute'])
 					break;
 			}
 		}
+		$scope.levelup = function(List, Champ){
+			switch($scope.roleSel.value){
+				case "mid":
+					$scope.mid[List.title-1].champs.splice(findWithAttr(List.champs,"champion",Champ.champion),1);
+					Champ.level++;
+					$scope.mid[List.title].champs.push(Champ);
+					break;
+				case "adc":
+					$scope.adc[List.title-1].champs.splice(findWithAttr(List.champs,"champion",Champ.champion),1);
+					Champ.level++;
+					$scope.adc[List.title].champs.push(Champ);
+					break;
+				case "top":
+					$scope.top[List.title-1].champs.splice(findWithAttr(List.champs,"champion",Champ.champion),1);
+					Champ.level++;
+					$scope.top[List.title].champs.push(Champ);
+					break;
+			}
+		}
 		$scope.desc=["Achieve 95% of max CS with full runes and masteries, no lane opponents, and normal items. This will let you CS almost perfectly when no one is bothering you, and makes it a huge problem for the enemy team to leave you alone in lane for long periods of time, as you will get farmed very quickly relative to other players.",
 					"Achieve 95% of max CS with full runes and masteries, no lane opponents, and normal items, but this time, keep moving between each auto attack. Stay out of auto attack range of the minions before you last hit, and go in for the CS only when you will one-hit the minion. This exercise will naturally increase your mechanical ability, and by enhancing your mobility, will allow you to remain safer in lane. (Note that you can cancel the second half of the auto attack animation by clicking immediately after the damage applies to the minion for melee champions, and after the projectile leaves your character for ranged champions. For ranged champions it can look like the ranged projectile has left the character, but when it arrives it does no damage to the minion. This is because you canceled the animation a hair too early)",
-					]
+					];
 		Main.setUpChampions().then(function(success){
-			$scope.adc={levelZero: {title:"Level One", desc:$scope.desc[0],champs:success.adc},LevelOne: {title:"Level Two", desc:$scope.desc[1], champs:[]},LevelTwo: {title:"Level Three", desc:'', champs:[]},LevelThree: {title:"Level Four", desc:'', champs:[]},LevelFour: {title:"Level Five", desc:'', champs:[]},LevelFive: {title:"Level Six", desc:'', champs:[]},LevelSix: {title:"Level Seven", desc:'', champs:[]},LevelSeven: {title:"Level Eight", desc:'', champs:[]},LevelEight: {title:"Level Nine", desc:'', champs:[]},LevelNine: {title:"Level Ten", desc:'', champs:[]},LevelTen: {title:"Level Eleven", desc:'', champs:[]},LevelEleven: {title:"Level Twelve", desc:'', champs:[]},LevelTwelve: {title:"Level Thirteen", desc:'', champs:[]},LevelThirteen: {title:"Level FourTeen", desc:'', champs:[]}};
-			$scope.mid={levelZero: {title:"Level One", desc:$scope.desc[0],champs:success.mid},LevelOne: {title:"Level Two", desc:$scope.desc[1], champs:[]},LevelTwo: {title:"Level Three", desc:'', champs:[]},LevelThree: {title:"Level Four", desc:'', champs:[]},LevelFour: {title:"Level Five", desc:'', champs:[]},LevelFive: {title:"Level Six", desc:'', champs:[]},LevelSix: {title:"Level Seven", desc:'', champs:[]},LevelSeven: {title:"Level Eight", desc:'', champs:[]},LevelEight: {title:"Level Nine", desc:'', champs:[]},LevelNine: {title:"Level Ten", desc:'', champs:[]},LevelTen: {title:"Level Eleven", desc:'', champs:[]},LevelEleven: {title:"Level Twelve", desc:'', champs:[]},LevelTwelve: {title:"Level Thirteen", desc:'', champs:[]},LevelThirteen: {title:"Level FourTeen", desc:'', champs:[]}};
-			$scope.top={levelZero: {title:"Level One", desc:$scope.desc[0],champs:success.top},LevelOne: {title:"Level Two", desc:$scope.desc[1], champs:[]},LevelTwo: {title:"Level Three", desc:'', champs:[]},LevelThree: {title:"Level Four", desc:'', champs:[]},LevelFour: {title:"Level Five", desc:'', champs:[]},LevelFive: {title:"Level Six", desc:'', champs:[]},LevelSix: {title:"Level Seven", desc:'', champs:[]},LevelSeven: {title:"Level Eight", desc:'', champs:[]},LevelEight: {title:"Level Nine", desc:'', champs:[]},LevelNine: {title:"Level Ten", desc:'', champs:[]},LevelTen: {title:"Level Eleven", desc:'', champs:[]},LevelEleven: {title:"Level Twelve", desc:'', champs:[]},LevelTwelve: {title:"Level Thirteen", desc:'', champs:[]},LevelThirteen: {title:"Level FourTeen", desc:'', champs:[]}};
+			$scope.adc={levelZero: {title: 1, desc:$scope.desc[0],champs:success.adc},LevelOne: {title:2, desc:$scope.desc[1], champs:[]},LevelTwo: {title:3, desc:'', champs:[]},LevelThree: {title:4, desc:'', champs:[]},LevelFour: {title:5, desc:'', champs:[]},LevelFive: {title:6, desc:'', champs:[]},LevelSix: {title:7, desc:'', champs:[]},LevelSeven: {title:8, desc:'', champs:[]},LevelEight: {title:9, desc:'', champs:[]},LevelNine: {title:10, desc:'', champs:[]},LevelTen: {title:11, desc:'', champs:[]},LevelEleven: {title:12, desc:'', champs:[]},LevelTwelve: {title:13, desc:'', champs:[]},LevelThirteen: {title:14, desc:'', champs:[]}};
+			$scope.mid={levelZero: {title: 1, desc:$scope.desc[0],champs:success.mid},LevelOne: {title:2, desc:$scope.desc[1], champs:[]},LevelTwo: {title:3, desc:'', champs:[]},LevelThree: {title:4, desc:'', champs:[]},LevelFour: {title:5, desc:'', champs:[]},LevelFive: {title:6, desc:'', champs:[]},LevelSix: {title:7, desc:'', champs:[]},LevelSeven: {title:8, desc:'', champs:[]},LevelEight: {title:9, desc:'', champs:[]},LevelNine: {title:10, desc:'', champs:[]},LevelTen: {title:11, desc:'', champs:[]},LevelEleven: {title:12, desc:'', champs:[]},LevelTwelve: {title:13, desc:'', champs:[]},LevelThirteen: {title:14, desc:'', champs:[]}};
+			$scope.top={levelZero: {title: 1, desc:$scope.desc[0],champs:success.top},LevelOne: {title:2, desc:$scope.desc[1], champs:[]},LevelTwo: {title:3, desc:'', champs:[]},LevelThree: {title:4, desc:'', champs:[]},LevelFour: {title:5, desc:'', champs:[]},LevelFive: {title:6, desc:'', champs:[]},LevelSix: {title:7, desc:'', champs:[]},LevelSeven: {title:8, desc:'', champs:[]},LevelEight: {title:9, desc:'', champs:[]},LevelNine: {title:10, desc:'', champs:[]},LevelTen: {title:11, desc:'', champs:[]},LevelEleven: {title:12, desc:'', champs:[]},LevelTwelve: {title:13, desc:'', champs:[]},LevelThirteen: {title:14, desc:'', champs:[]}};
 		});
 }])
